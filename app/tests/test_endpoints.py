79650fe00bd21124332c315e7e1e7f3e1c5456f8
nikola <nikola.mitrovic@frontedgeit.se>
2022-10-29 23:37:05 +0200

    authorization token

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -20,6 +20,13 @@ def test_invalid_file_upload_error():
     assert "application/json" in response.headers['content-type']
 
 
+def test_prediction_upload_missing_headers():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        response = client.post("/", files={"file": open(path, 'rb')})
+        assert response.status_code == 401
+
+
 def test_prediction_upload():
     img_saved_path = BASE_DIR / "images"
     settings = get_settings()
@@ -28,10 +35,11 @@ def test_prediction_upload():
             img = Image.open(path)
         except:
             img = None
-        response = client.post("/", files={"file": open(path, 'rb')},
-                               headers={"Authorization": f"JWT {settings.app_auth_token}"}
-                               )
-
+        response = client.post(
+            "/",
+            files={"file": open(path, 'rb')},
+            headers={"Authorization": f"JWT {settings.app_auth_token}"}
+        )
         if img is None:
             assert response.status_code == 400
         else:
